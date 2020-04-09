4d6c25a88b9cc761b0e3094409cc4a3eb362e9a3
João Harbs <harbsprog@gmail.com>
2020-04-08 23:45:00 -0300

    Create update route and small adjustments

--- src/app.js
+++ src/app.js
@@ -7,28 +7,28 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const repositories = []
+let repositories = []
 
 function verifyIsArray(request, response, next) {
   const { techs } = request.body
 
-  if (Array.isArray(techs)) {
-    next()
+  if (!Array.isArray(techs)) {
+    return response
+      .status(400)
+      .json({ message: 'The techs field needs to be an array :)' })
   }
 
-  return response
-    .status(400)
-    .json({ message: 'The techs field needs to be an array :)' })
+  return next()
 }
 
 function verifyIsUuid(request, response, next) {
   const { id } = request.params
 
-  if (isUuid(id)) {
-    next()
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: 'The Uuid is not valid :(' })
   }
 
-  return response.status(400).json({ message: 'The Uuid is not valid :(' })
+  return next()
 }
 
 app.use('/repositories/:id', verifyIsUuid)
@@ -40,24 +40,38 @@ app.get('/repositories', (request, response) => {
 app.post('/repositories', verifyIsArray, (request, response) => {
   const { title, url, techs } = request.body
 
-  console.log(title, url, techs)
-  if (title && url && techs) {
-    const repository = {
-      id: uuid(),
-      title: title,
-      url: url,
-      techs: techs,
-      likes: 0,
-    }
-    repositories.push(repository)
-    return response.status(200).json(repository)
+  if (!(title && url && techs)) {
+    return response.status(400).json({ message: 'Fill the requested fields!' })
   }
 
-  return response.status(400).json({ message: 'Fill the requested fields!' })
+  const repository = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0,
+  }
+
+  repositories.push(repository)
+
+  return response.status(200).json(repository)
 })
 
 app.put('/repositories/:id', (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { url, title, techs } = request.body
+
+  const uuid = repositories.findIndex((element) => element.id === id)
+
+  if (uuid < 0) {
+    return response.status(400).json({ message: 'Repository not found!' })
+  }
+
+  repositories[uuid].url = url
+  repositories[uuid].title = title
+  repositories[uuid].techs = techs
+
+  return response.status(200).json(repositories[uuid])
 })
 
 app.delete('/repositories/:id', (request, response) => {
@@ -71,11 +85,21 @@ app.delete('/repositories/:id', (request, response) => {
 
   repositories.splice(uuid, 1)
 
-  return response.status(200).send()
+  return response.status(204).send()
 })
 
 app.post('/repositories/:id/like', (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const uuid = repositories.findIndex((element) => element.id === id)
+
+  if (uuid < 0) {
+    return response.status(400).json({ message: 'Repository not found!' })
+  }
+
+  repositories[uuid].likes++
+
+  return response.status(200).json(repositories[uuid])
 })
 
 module.exports = app
